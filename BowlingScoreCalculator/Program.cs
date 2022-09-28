7f55cf5265a426fd6331c34da9aaa72276cc264d
DanielHendriksen <daynee_93@hotmail.com>
2022-09-28 20:42:01 +0200

    Changed throwsList input to be valid on execution

--- BowlingScoreCalculator/Program.cs
+++ BowlingScoreCalculator/Program.cs
@@ -12,7 +12,7 @@ namespace BowlingScoreCalculator
             Console.WriteLine("Welcome to Daniels bowling game 2022, press any key to begin the game");
             Console.Read();
 
-            List<int> throwsList = new List<int> { 8, 2, 3, 0, 7, 0, 10, 0, 0, 3, 1, 6, 2, 5, 0, 5, 2, 7, 5, 8, 8, 2, 6};
+            List<int> throwsList = new List<int> { 8, 2, 3, 0, 7, 0, 10, 0, 0, 3, 1, 6, 2, 5, 0, 5, 2, 7, 5, 8, 8};
             BowlingGame game = new BowlingGame(throwsList);
 
             game.RunGame();
