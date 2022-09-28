19c829b7577a131bf7abe8097a67fe158a5402dc
DanielHendriksen <daynee_93@hotmail.com>
2022-09-28 20:45:58 +0200

    Changed data input to throwsList

--- BowlingScoreCalculator/Program.cs
+++ BowlingScoreCalculator/Program.cs
@@ -12,7 +12,7 @@ namespace BowlingScoreCalculator
             Console.WriteLine("Welcome to Daniels bowling game 2022, press any key to begin the game");
             Console.Read();
 
-            List<int> throwsList = new List<int> { 8, 2, 3, 0, 7, 0, 10, 0, 0, 3, 1, 6, 2, 5, 0, 5, 2, 7, 5, 8, 8};
+            List<int> throwsList = new List<int> {2, 7, 1, 5, 7, 2, 9, 0, 3, 4, 4, 4, 9, 0, 8, 1, 6, 4, 6, 4, 2};
             BowlingGame game = new BowlingGame(throwsList);
 
             game.RunGame();
